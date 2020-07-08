e2f71b80e75f49c4345739e0b1a34575c74426c7
mohit200507 <64905309+mohit200507@users.noreply.github.com>
2020-07-08 22:04:21 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,17 @@
+class Plinko {
+    constructor (x,y,r){
+        var options = {
+         isStatic:true
+        }
+        this.body = Bodies.circle (x,y,r,options);
+        this.r = r;
+    }
+
+ display () {
+     
+ }
+
+
+
+
+}
